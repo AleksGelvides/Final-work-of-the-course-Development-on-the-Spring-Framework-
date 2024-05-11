671f82851f46c50470d474b2c8395abc72cb7102
Aleksey Gelvides <82328608+AleksGelvides@users.noreply.github.com>
2024-05-11 12:58:24 +0700

    init project

---
+++ src/test/java/com/finalProject/finalProjectDevOnSpring/FinalProjectDevOnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalProject.finalProjectDevOnSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectDevOnSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
